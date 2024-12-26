a62086c1164088ee63e4530711165881d1d904b1
SnarkyDeveloper <TheCoderKid@proton.me>
2024-12-26 05:15:02 -0800

    Oops crap i missed a change :3

--- Backend/Modules/choose.py
+++ Backend/Modules/choose.py
@@ -1,6 +1,5 @@
 import random
 from discord.ext import commands
-from Backend.utils import check_permissions
 
 class Choose(commands.Cog):
     def __init__(self, bot):
@@ -9,11 +8,7 @@ class Choose(commands.Cog):
 
     @commands.command(description='For when you wanna settle the score some other way')
     async def choose(self, ctx, choices: str):
-        if check_permissions(ctx.author):
-            choice_list = choices.split()
-            await ctx.send(random.choice(choice_list))
-        else:
-            pass
-
+        choice_list = choices.split()
+        await ctx.send(random.choice(choice_list))
 async def setup(bot):
     await bot.add_cog(Choose(bot))
