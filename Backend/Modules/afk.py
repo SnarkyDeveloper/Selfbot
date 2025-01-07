b0e63013d2288a4c366d2d9c2a1e87fc64c787d5
SnarkyDeveloper <TheCoderKid@proton.me>
2025-01-07 10:15:47 -0800

    afk works now :D

--- Backend/Modules/afk.py
+++ Backend/Modules/afk.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 import time
 from Backend.send import send
+
 path = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 try:
@@ -45,6 +46,7 @@ class Afk(commands.Cog):
     async def on_message(self, message):
         if message.author.bot:
             return
+        ctx = await self.bot.get_context(message)
 
         with open(f'{path}/data/users/afk.json', 'r') as f:
             afk_data = json.load(f)
@@ -53,20 +55,21 @@ class Afk(commands.Cog):
             if not message.content.lower().startswith('>afk') and not message.content.lower().startswith('>brb'):
                 afk_time = round((time.time() * 1000 - afk_data[str(message.guild.id)][str(message.author.id)]['timestamp']) / 1000)
                 del afk_data[str(message.guild.id)][str(message.author.id)]
-
+                minutes, seconds = divmod(afk_time, 60)
+                hours, minutes = divmod(minutes, 60)
+                try:
+                    deletable = await send(self.bot, ctx, title='AFK', content=f"Welcome back {message.author.mention}! You were AFK for {hours} hours, {minutes} minutes, and {seconds} seconds.")
+                except Exception as e:
+                    print(f"Error: {e}")
                 with open(f'{path}/data/users/afk.json', 'w') as f:
                     json.dump(afk_data, f)
 
-                minutes, seconds = divmod(afk_time, 60)
-                hours, minutes = divmod(minutes, 60)
-
-                deletable = await send(self.bot, message, title='AFK', content=f"Welcome back {message.author.mention}! You were AFK for {hours} hours, {minutes} minutes, and {seconds} seconds.")
                 await deletable.delete(delay=15)
         if message.mentions:
             for user in message.mentions:
                 if str(user.id) in afk_data.get(str(message.guild.id), {}):
                     reason = afk_data[str(message.guild.id)][str(user.id)]['reason']
-                    await send(self.bot, message, title='AFK', content=f"{user.name} is AFK with reason: {reason}")
+                    await send(self.bot, ctx, title='AFK', content=f"{user.name} is AFK with reason: {reason}")
 
 async def setup(bot):
     await bot.add_cog(Afk(bot))
